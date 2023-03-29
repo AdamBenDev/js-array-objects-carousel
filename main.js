91da52d9d1539aed71a9ba6e130298973dde627b
AdamBenDev <adamaya9732@gmail.com>
2023-03-29 06:45:25 +0200

    function change image and functions autoplay

--- main.js
+++ main.js
@@ -31,3 +31,55 @@ const source = [
     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
   }
 ];
+
+
+
+// ! FUNZIONI
+// Funzione cambio immagine
+const changePic = target => {
+  
+    // Rimuovo la classe active all'immagine corrente
+    images[currentActiveIndex].classList.remove('active');
+    thumbs[currentActiveIndex].classList.remove('active');
+
+    if (target === 'next'){
+      // Incremento l'indice
+      currentActiveIndex++; 
+      // Controllo per ripartire dall'inizio
+      if(currentActiveIndex === images.length){
+      currentActiveIndex = 0;
+      }
+    } else if (target === 'prev'){
+      // Decremento l'indice
+      currentActiveIndex--; 
+      // Controllo per ripartire dalla fine
+      if(currentActiveIndex < 0){
+      currentActiveIndex = images.length - 1;
+      }
+    } else {
+      // Metto il current index sul thumb corrente
+      currentActiveIndex = target;
+    }
+
+    // Assegno la classe active al nuovo indice
+    images[currentActiveIndex].classList.add('active');
+    thumbs[currentActiveIndex].classList.add('active');
+}
+
+
+// Funzione start autoplay
+const startAutoplay = (direction) => {
+    autoplay = setInterval(()=>{
+      changePic(direction);
+    }, 3000);
+  };
+  
+  // Funzione stop autoplay
+  const stopAutoplay = (changeButton = true) => {
+    isPlaying = false;
+    clearInterval(autoplay);
+    
+    if(changeButton) {
+      autoplayButton.innerText = 'RESUME AUTOPLAY';
+    }
+  };
